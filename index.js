b8777a05fad55975d25a56094a57b57fe95b6523
DenGian <97441878+DenGian@users.noreply.github.com>
2022-05-16 17:01:52 +0200

    add times 2 button

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 var camel = 0;
 var clicks = 1;
-
+var jamalCost = 100;
+var jamals = 0;
 
 document.getElementById('camel').addEventListener('click', addCamel);
-function addCamel(){
-camel=camel+clicks;
-document.getElementById('score').innerHTML=camel.toFixed(2)
+
+function addCamel() {
+    camel = camel + clicks;
+    document.getElementById('score').innerHTML = camel.toFixed(2)
+}
+document.getElementById('jamal').addEventListener('click',upgrade1);
+
+function upgrade1(){
+    if(camel>=jamalCost){
+        camel=camel-jamalCost;
+        jamals=jamals+1;
+        jamalCost=Math.round(jamalCost*1.3);
+        clicks=clicks+1;
+        document.getElementById('score').innerHTML = camel.toFixed(2);
+        document.getElementById('jamalCost').innerHTML = jamalCost.toFixed(2);
+        document.getElementById('jamals').innerHTML = jamals;
+    }
 }
